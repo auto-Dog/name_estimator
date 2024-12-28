08ece18b7f20da29ea0f243a3e17f972b4e26c92
auto-Dog <2969860272@qq.com>
2024-12-29 00:28:42 +0800

    update dataloader for balance, update L2 norm in loss

--- dataloaders/make_data.py
+++ dataloaders/make_data.py
@@ -14,8 +14,7 @@ import json
 from tqdm import tqdm
 from imblearn.under_sampling import RandomUnderSampler
 
-df = pd.read_excel('./name_table.xlsx',index_col='Colorname')  # 替换为您的文件路径
-
+df = pd.read_excel('../name_table.xlsx',index_col='Colorname')  # 替换为您的文件路径
 # 初始化字典
 color_name = []
 color_value = []
@@ -77,32 +76,38 @@ class CVDImageNet(ImageFolder):
         return img, path
 
 trainset = CVDImageNet('/work/mingjundu/imagenet100k/',split='imagenet_subtrain',patch_size=16,img_size=32,cvd='deutan')
-valset = CVDImageNet('/work/mingjundu/imagenet100k/',split='imagenet_subtest',patch_size=16,img_size=32,cvd='deutan')
-trainloader = torch.utils.data.DataLoader(trainset,batch_size=1,shuffle = True,num_workers=4)
-valloader = torch.utils.data.DataLoader(valset,batch_size=1,shuffle = True,num_workers=4)
+valset = CVDImageNet('/work/mingjundu/imagenet100k/',split='imagenet_subval',patch_size=16,img_size=32,cvd='deutan')
+trainloader = torch.utils.data.DataLoader(trainset,batch_size=1,shuffle = True,num_workers=8)
+valloader = torch.utils.data.DataLoader(valset,batch_size=1,shuffle = True,num_workers=8)
 
-X_train = []
-Y_train = []
-row_index = 0
-for img,path in tqdm(trainloader):
-    for i in range(5):  # 每张图采样五个颜色点，实际可能不会全部采纳
-        color_names = [-1,-1,-1, -1,-1,-1, -1,-1,-1]
-        x_index = torch.randint(1,30)
-        y_index = torch.randint(1,30)
-        surroundings = img[0, :, x_index-1:x_index+2, y_index-1:y_index+2].reshape(3,-1)
-        for j in range(9):
-            color_names[j] = classify_color(surroundings[:,j].flatten().numpy()*255)
-        target = color_names[4]
-        target_patch_id = x_index*32+y_index
-        mask = (color_names==target)
-        if np.sum(mask)>=3: # voting,至少有3/9个像素支持当前颜色
-            X_train.append((path,target_patch_id))
-            Y_train.append(target)
-        
-under_sampler = RandomUnderSampler(random_state=1337)
-X_resampled, Y_resampled = under_sampler.fit_resample(X_train,Y_train)
-print('Resample result:',Counter(Y_resampled))
-all_path,all_patch_id = zip(*X_resampled)
-df_data = {'Path':all_path,'Patch_ID':all_patch_id,'Color_ID':Y_resampled}
-df_train = pd.DataFrame(data=df_data)
-df_train.to_csv('train_label.csv')
+def make_data_label(loader,filename):
+    X_train = []
+    Y_train = []
+    for img,path in tqdm(loader):
+        for i in range(10):  # 每张图采样十个颜色点，实际可能不会全部采纳
+            color_names = np.array([-1,-1,-1, -1,-1,-1, -1,-1,-1])
+            x_index = np.random.randint(1,30)
+            y_index = np.random.randint(1,30)
+            surroundings = img[0, :, x_index-1:x_index+2, y_index-1:y_index+2].reshape(3,-1)
+            for j in range(9):
+                color_names[j] = classify_color(surroundings[:,j].flatten().numpy()*255)
+            target = color_names[4]
+            target_patch_id = x_index*32+y_index
+            mask = (color_names==target)
+            if np.sum(mask)>=3: # voting,至少有3/9个像素支持当前颜色
+                X_train.append((path[0],target_patch_id))
+                Y_train.append(target)
+        # if len(Y_train)>1000:   # debug
+        #     time.sleep(0.5)
+        #     break
+            
+    under_sampler = RandomUnderSampler(random_state=1337)
+    X_resampled, Y_resampled = under_sampler.fit_resample(X_train,Y_train)
+    print('Resample result:',Counter(Y_resampled))
+    all_path,all_patch_id = zip(*X_resampled)
+    df_data = {'Path':all_path,'Patch_ID':all_patch_id,'Color_ID':Y_resampled}
+    df_train = pd.DataFrame(data=df_data)
+    df_train.to_csv(filename)
+
+make_data_label(trainloader,'train_label.csv')
+make_data_label(valloader,'val_label.csv')
--- train_deu.sh
+++ train_deu.sh
@@ -4,4 +4,4 @@
 #SBATCH -c 8
 #SBATCH --gres=gpu:2
 
-python train.py --prefix vit_cn3 --size 512 --patch 16 --batchsize 32
+python train.py --prefix vit_cn4 --size 512 --patch 16 --batchsize 32
